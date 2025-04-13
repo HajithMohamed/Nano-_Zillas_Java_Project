35ca3340f33c4000b80b333d0d53ef06267025bb
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2025-04-13 20:30:39 +0530

    Login page UI completed

--- src/main/java/org/example/mini_project_java/App.java
+++ src/main/java/org/example/mini_project_java/App.java
@@ -2,15 +2,20 @@ package org.example.mini_project_java;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Login.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Login");
-        primaryStage.setScene(loader.load());
         primaryStage.show();
     }
 }
