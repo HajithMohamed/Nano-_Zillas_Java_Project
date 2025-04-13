725de722034384ac315d38409f8870d65b0f7d8f
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2025-04-13 07:20:28 +0530

    Users class defined

--- src/main/java/org/example/mini_project_java/Models/Users.java
+++ src/main/java/org/example/mini_project_java/Models/Users.java
@@ -1,4 +1,40 @@
 package org.example.mini_project_java.Models;
 
-public class Users {
+abstract public class Users {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String role;
+
+    public Users(String username, String password, String name, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void login(){
+
+    }
+    public void logout(){
+
+    }
+    abstract  void updateProfile();
+
 }
