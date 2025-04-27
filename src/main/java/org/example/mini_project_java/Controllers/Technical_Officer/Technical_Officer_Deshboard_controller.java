2927e8d1c614245e790a171a109e2ddac3817f7e
AkMJafran <mohamedjafran300@gmail.com>
2025-04-27 15:49:24 +0530

    commit

--- src/main/java/org/example/mini_project_java/Controllers/Technical_Officer/Technical_Officer_Deshboard_controller.java
+++ src/main/java/org/example/mini_project_java/Controllers/Technical_Officer/Technical_Officer_Deshboard_controller.java
@@ -1,9 +1,5 @@
 package org.example.mini_project_java.Controllers.Technical_Officer;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-
-public class Technical_Officer_Deshboard {
+public class Technical_Officer_Deshboard_controller {
 
 }
