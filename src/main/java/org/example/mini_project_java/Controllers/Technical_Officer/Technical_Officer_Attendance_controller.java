54620aab6551b7462bcc48e8cd336dbea6a1426c
AkMJafran <mohamedjafran300@gmail.com>
2025-04-27 13:08:35 +0530

    gfgfgfgfgg

--- src/main/java/org/example/mini_project_java/Controllers/Technical_Officer/Technical_Officer_Attendance_controller.java
+++ src/main/java/org/example/mini_project_java/Controllers/Technical_Officer/Technical_Officer_Attendance_controller.java
@@ -1,9 +1,17 @@
 package org.example.mini_project_java.Controllers.Technical_Officer;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class Technical_Officer_Attendance_controller {
     public AnchorPane techAttendance;
     public TextField courseIdField;
+    public TextField courseNameField;
+
+    public TableView attendanceTable;
+    public TableColumn studentIdColumn;
+    public TableColumn studentNameColumn;
+    public TableColumn attendanceColumn;
 }
